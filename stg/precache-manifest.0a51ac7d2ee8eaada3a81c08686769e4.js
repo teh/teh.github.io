a2d5a1ac1aa0e2f28fc65e6596863a75739666a2
Tom Hunger <tehunger@gmail.com>
2020-02-03 16:14:46 +0000

    publish

---
+++ stg/precache-manifest.0a51ac7d2ee8eaada3a81c08686769e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846925ec73247206bc9686e7a86d2124",
+    "url": "/stg/index.html"
+  },
+  {
+    "revision": "406453fc4b59dd84adbf",
+    "url": "/stg/static/css/main.96043cac.chunk.css"
+  },
+  {
+    "revision": "93ea2fb98c210adacb7a",
+    "url": "/stg/static/js/2.6c02b480.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stg/static/js/2.6c02b480.chunk.js.LICENSE"
+  },
+  {
+    "revision": "406453fc4b59dd84adbf",
+    "url": "/stg/static/js/main.3ae42999.chunk.js"
+  },
+  {
+    "revision": "556308aca83c92d8099c",
+    "url": "/stg/static/js/runtime-main.d74ba3db.js"
+  }
+]);
